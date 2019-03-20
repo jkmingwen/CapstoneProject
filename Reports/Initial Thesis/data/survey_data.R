dec4611859c69bf9e887adb9ff2cbf25599099d8
Jaime Koh <jkmingwen@gmail.com>
2019-03-20 21:59:40 +0800

    Update radar graphs using fmsb

--- Reports/Initial Thesis/data/survey_data.R
+++ Reports/Initial Thesis/data/survey_data.R
@@ -2,6 +2,15 @@
 library(tidyr)  # for easy restructuring of data structures
 library(RColorBrewer)  # for colours
 library(plotly)  # for radar graphs
+library(fmsb)  # alternate radar graphs
+
+add.alpha <- function(col, alpha=1) {  # function to add alpha to rcolorbrewer
+  if(missing(col))
+    stop("Please provide a vector of colours.")
+  apply(sapply(col, col2rgb)/255, 2, 
+        function(x) 
+          rgb(x[1], x[2], x[3], alpha=alpha))  
+}
 
 ## Set working directory
 setwd("~/Documents/CapstoneProject/Reports/Initial Thesis/data/")
@@ -32,6 +41,10 @@ intuitivity_mean <- c(mean(quant_intuitivity[,2]),
                       mean(quant_intuitivity[,3]),
                       mean(quant_intuitivity[,4]))
 
+# Convert to data frames
+quant_engagement <- as.data.frame(quant_engagement)
+quant_intuitivity <- as.data.frame(quant_intuitivity)
+
 # Restructure data for grouped bar graph
 # combine separate animation columns into one "Animation" column (wide to long)
 quant_engagement <- gather(as.data.frame(quant_engagement), 
@@ -45,10 +58,6 @@ quant_intuitivity <- gather(as.data.frame(quant_intuitivity),
 # reshape from long to wide with Animations as rows and participants as cols
 quant_engagement <- spread(as.data.frame(quant_engagement), Participant, Rating)
 quant_intuitivity <- spread(as.data.frame(quant_intuitivity), Participant, Rating)
-
-# summary(sapply(quant_engagement[,2:13], as.numeric)[1,])
-# summary(sapply(quant_engagement[,2:13], as.numeric)[2,])
-# summary(sapply(quant_engagement[,2:13], as.numeric)[3,])
 
 # Visualise quantitative data
 graph_cols <- brewer.pal(3, "Accent")  # set colour selection
@@ -87,68 +96,103 @@ rownames(qual_improvement) <- qual_improvement[[1]]
 qual_engagement <- qual_engagement[-1]
 qual_intuitivity <- qual_intuitivity[-1]
 qual_improvement <- qual_improvement[-1]
-# data ready for visualisations
+# reshape and change to numeric type
+qual_engagement <- as.data.frame(t(qual_engagement))
+qual_engagement[] <- lapply(qual_engagement, 
+                            function(x) as.numeric(as.character(x)))
+qual_intuitivity <- as.data.frame(t(qual_intuitivity))
+qual_intuitivity[] <- lapply(qual_intuitivity, 
+                            function(x) as.numeric(as.character(x)))
+qual_improvement <- as.data.frame(t(qual_improvement))
+qual_improvement[] <- lapply(qual_improvement, 
+                            function(x) as.numeric(as.character(x)))
+# Combine maxmin into data frame
+qual_engagement <- rbind(rep(max(qual_engagement), ncol(qual_engagement)),
+                         rep(0, ncol(qual_engagement)),
+                         qual_engagement)
+qual_intuitivity <- rbind(rep(max(qual_intuitivity), ncol(qual_intuitivity)),
+                         rep(0, ncol(qual_intuitivity)),
+                         qual_intuitivity)
+qual_improvement <- rbind(rep(max(qual_improvement), ncol(qual_improvement)),
+                         rep(0, ncol(qual_improvement)),
+                         qual_improvement)
+### data ready for visualisations! ###
 
 # Visualise qualitative data with radar graphs
-# "Engagement" radar graph
-plot_ly(type = 'scatterpolar',
-        fill = 'toself') %>% 
-  add_trace(r = qual_engagement[[1]],
-            theta = rownames(qual_engagement),
-            color = graph_cols[1],
-            name = 'Circles') %>%
-  add_trace(r = qual_engagement[[2]],
-            theta = rownames(qual_engagement),
-            color = graph_cols[2],
-            name = 'Clouds') %>%
-  add_trace(r = qual_engagement[[3]],
-            theta = rownames(qual_engagement),
-            color = graph_cols[3],
-            name = 'Shimmer') %>%
-  layout(polar = 
-           list(radialaxis = 
-                  list(visible = T, 
-                       range = c(0, as.numeric(max(unlist(qual_engagement[1:3])))))),
-         title = "Engagement")
+graph_cols_alpha <- add.alpha(graph_cols, 0.5)
+## Engagement
+radarchart(qual_engagement[1:3,], axistype = 1,  # Circles
+           pcol = graph_cols[1], pfcol = graph_cols_alpha[1], plwd = 3,
+           seg = max(qual_engagement),
+           caxislabels = 0:max(qual_engagement), 
+           vlcex = 1.5, axislabcol = "black",
+           cglwd = 2, cglcol = "darkgray", cglty = 1,
+           title(main = "Circles: Tag counts for 'Engagement'", cex.main = 2))
 
-# "Intuitiveness" radar graph
-plot_ly(type = 'scatterpolar',
-        fill = 'toself') %>% 
-  add_trace(r = qual_intuitivity[[1]],
-            theta = rownames(qual_intuitivity),
-            color = graph_cols[1],
-            name = 'Circles') %>%
-  add_trace(r = qual_intuitivity[[2]],
-            theta = rownames(qual_intuitivity),
-            color = graph_cols[2],
-            name = 'Clouds') %>%
-  add_trace(r = qual_intuitivity[[3]],
-            theta = rownames(qual_intuitivity),
-            color = graph_cols[3],
-            name = 'Shimmer') %>%
-  layout(polar = 
-           list(radialaxis = 
-                  list(visible = T, 
-                       range = c(0, as.numeric(max(unlist(qual_intuitivity[1:3])))))),
-         title = "Intuitiveness")
+radarchart(qual_engagement[c(1,2,4),], axistype = 1,  # Clouds
+           pcol = graph_cols[2], pfcol = graph_cols_alpha[2], plwd = 3,
+           seg = max(qual_engagement),
+           caxislabels = 0:max(qual_engagement), 
+           vlcex = 1.5, axislabcol = "black",
+           cglwd = 2, cglcol = "darkgray", cglty = 1,
+           title(main = "Clouds: Tag counts for 'Engagement'", cex.main = 2))
 
-# "improvement" radar graph
-plot_ly(type = 'scatterpolar',
-        fill = 'toself') %>% 
-  add_trace(r = qual_improvement[[1]],
-            theta = rownames(qual_improvement),
-            color = graph_cols[1],
-            name = 'Circles') %>%
-  add_trace(r = qual_improvement[[2]],
-            theta = rownames(qual_improvement),
-            color = graph_cols[2],
-            name = 'Clouds') %>%
-  add_trace(r = qual_improvement[[3]],
-            theta = rownames(qual_improvement),
-            color = graph_cols[3],
-            name = 'Shimmer') %>%
-  layout(polar = 
-           list(radialaxis = 
-                  list(visible = T, 
-                       range = c(0, as.numeric(max(unlist(qual_improvement[1:3])))))),
-         title = "Improvements")
+radarchart(qual_engagement[c(1,2,5),], axistype = 1,  # Shimmer
+           pcol = graph_cols[3], pfcol = graph_cols_alpha[3], plwd = 3,
+           seg = max(qual_engagement),
+           caxislabels = 0:max(qual_engagement), 
+           vlcex = 1.5, axislabcol = "black",
+           cglwd = 2, cglcol = "darkgray", cglty = 1,
+           title(main = "Shimmer: Tag counts for 'Engagement'", cex.main = 2))
+
+## Intuitivity
+radarchart(qual_intuitivity[1:3,], axistype = 1,  # Circles
+           pcol = graph_cols[1], pfcol = graph_cols_alpha[1], plwd = 3,
+           seg = max(qual_intuitivity),
+           caxislabels = 0:max(qual_intuitivity), 
+           vlcex = 1.5, axislabcol = "black",
+           cglwd = 2, cglcol = "darkgray", cglty = 1,
+           title(main = "Circles: Tag counts for 'Intuitiveness'", cex.main = 2))
+
+radarchart(qual_intuitivity[c(1,2,4),], axistype = 1,  # Clouds
+           pcol = graph_cols[2], pfcol = graph_cols_alpha[2], plwd = 3,
+           seg = max(qual_intuitivity),
+           caxislabels = 0:max(qual_intuitivity), 
+           vlcex = 1.5, axislabcol = "black",
+           cglwd = 2, cglcol = "darkgray", cglty = 1,
+           title(main = "Clouds: Tag counts for 'Intuitiveness'", cex.main = 2))
+
+radarchart(qual_intuitivity[c(1,2,5),], axistype = 1,  # Shimmer
+           pcol = graph_cols[3], pfcol = graph_cols_alpha[3], plwd = 3,
+           seg = max(qual_intuitivity),
+           caxislabels = 0:max(qual_intuitivity), 
+           vlcex = 1.5, axislabcol = "black",
+           cglwd = 2, cglcol = "darkgray", cglty = 1,
+           title(main = "Shimmer: Tag counts for 'Intuitiveness'", cex.main = 2))
+
+## Improvement
+radarchart(qual_improvement[1:3,], axistype = 1,  # Circles
+           pcol = graph_cols[1], pfcol = graph_cols_alpha[1], plwd = 3,
+           seg = max(qual_improvement),
+           caxislabels = 0:max(qual_improvement), 
+           vlcex = 1.5, axislabcol = "black",
+           cglwd = 2, cglcol = "darkgray", cglty = 1,
+           title(main = "Circles: Tag counts for 'Improvement'", cex.main = 2))
+
+radarchart(qual_improvement[c(1,2,4),], axistype = 1,  # Clouds
+           pcol = graph_cols[2], pfcol = graph_cols_alpha[2], plwd = 3,
+           seg = max(qual_improvement),
+           caxislabels = 0:max(qual_improvement), 
+           vlcex = 1.5, axislabcol = "black",
+           cglwd = 2, cglcol = "darkgray", cglty = 1,
+           title(main = "Clouds: Tag counts for 'Improvement'", cex.main = 2))
+
+radarchart(qual_improvement[c(1,2,5),], axistype = 1,  # Shimmer
+           pcol = graph_cols[3], pfcol = graph_cols_alpha[3], plwd = 3,
+           seg = max(qual_improvement),
+           caxislabels = 0:max(qual_improvement), 
+           vlcex = 1.5, axislabcol = "black",
+           cglwd = 2, cglcol = "darkgray", cglty = 1,
+           title(main = "Shimmer: Tag counts for 'Improvement'", cex.main = 2))
+
+
